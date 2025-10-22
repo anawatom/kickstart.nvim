89e8e6a86b20ce4114bf6c546844c48126ad1877
Anawat Onmee <anawat@thinkbluedata.com>
2025-10-22 09:47:31 +0700

    Update theme

--- init.lua
+++ init.lua
@@ -1059,4 +1059,4 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme 'catppuccin-mocha'
